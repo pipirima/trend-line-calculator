68b9da5facbab02c7d5cdea5004b5e2f8fe78491
Wojciech Peisert <wojtekp@sauron.pl>
2020-08-16 15:15:29 +0200

    Change user in eocker commands

--- cli/build-env.sh
+++ cli/build-env.sh
@@ -2,4 +2,5 @@
 
 # This file builds package
 
-docker-compose exec -u application package composer install
+#docker-compose exec -u application package composer install
+docker-compose exec package composer install
