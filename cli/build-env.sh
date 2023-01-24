3c667e197607c0eb114c34fd7508a4bea9ba0a2f
Wojciech Peisert <wojtekp@sauron.pl>
2023-01-24 16:20:09 +0100

    some cleanup

--- cli/build-env.sh
+++ cli/build-env.sh
@@ -3,4 +3,4 @@
 # This file builds package
 
 #docker-compose exec -u application package composer install
-docker-compose exec -T package composer install
+docker-compose exec -T package composer build-env
